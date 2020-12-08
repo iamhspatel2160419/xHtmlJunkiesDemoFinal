ac1f9b304fbb123d80ac717586a88c9fb6c49fe1
“hardik” <hardik.s.patel@cygnetinfotech.com>
2020-12-08 14:20:44 +0530

    Initial Commit

---
+++ xHtmlJunkiesDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xHtmlJunkiesDemo
+//
+//  Created by Apple on 08/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
